5fb9253550f9cba2f73671b66017550a060076c9
Alexander Boyko <alexander.boyko@akvelon.com>
2023-07-23 17:21:29 -0700

    Add dispatch call to display kits list on dashboard

--- app/screens/DashboardScreen.jsx
+++ app/screens/DashboardScreen.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   StatusBar,
   Text,
   SafeAreaView,
-  Dimensions
+  View,
+  Dimensions,
+  ScrollView,
+  Button
 } from 'react-native';
+import { connect, useDispatch } from 'react-redux';
+import { getKits } from '../features/kits/kitsSlice';
+import axios from '../api/axios';
 
-export default DashboardScreen = () => {
+const DashboardScreen = (props) => {
+  const dispatch = useDispatch();
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    dispatch(getKits());
+  }, []);
+
+  useEffect(() => {
+    setList(props.list);
+  }, [props.list]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Dashboard</Text>
+      <ScrollView>
+        <View>
+          {props.list.map((kit) => {
+            return (
+              <View key={kit.id}>
+                <Text>{kit.id}</Text>
+                <Text>{kit.name}</Text>
+                <Text>{kit.desc}</Text>
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -24,3 +54,9 @@ const styles = StyleSheet.create({
     width: Dimensions.get('screen').width
   }
 });
+
+const mapStateToProps = (state) => {
+  return { list: state.kits.list };
+};
+
+export default connect(mapStateToProps)(DashboardScreen);
