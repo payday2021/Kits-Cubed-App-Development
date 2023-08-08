47b58fb861129f740083f7a4717a9b084f2619dd
nexusboyko <alexboyko@outlook.com>
2023-08-08 13:28:30 -0700

    Change axios base URL based on web or mobile

--- app/api/axios.js
+++ app/api/axios.js
@@ -1,10 +1,9 @@
 import Constants from 'expo-constants';
 import axios from 'axios';
 
-const { manifest } = Constants;
-const uri = `http://${Constants.expoConfig.debuggerHost
-  .split(':')
-  .shift()}:8080`;
+const uri = Constants.expoConfig.debuggerHost
+  ? `http://${Constants.expoConfig.debuggerHost.split(':').shift()}:8080` // if running on android emulator
+  : 'http://localhost:8080'; // if running on web
 
 export default axios.create({
   // baseURL: `http://192.168.1.52:8080`
