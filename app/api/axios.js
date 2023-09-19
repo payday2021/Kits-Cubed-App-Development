cf19c0cc2692b7bf80be482973a3027939bf203f
payday2021 <dylancratersun@gmail.com>
2023-09-19 16:48:31 -0700

    Update axios.js

--- app/api/axios.js
+++ app/api/axios.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const uri = Constants.expoConfig.debuggerHost
   ? `http://${Constants.expoConfig.debuggerHost.split(':').shift()}:8080` // if running on android emulator
   : 'http://localhost:8080'; // if running on web
+  
 
 export default axios.create({
   // baseURL: `http://192.168.1.52:8080`
