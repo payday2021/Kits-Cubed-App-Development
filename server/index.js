d04b17f1846ae719ff2450ab73baab471e920e54
payday2021 <dylancratersun@gmail.com>
2023-10-19 19:07:29 -0700

    Update server to include orders route

--- server/index.js
+++ server/index.js
@@ -5,20 +5,22 @@ const bodyParser = require('body-parser');
 const app = express();
 const kitsRouter = require('./routes/kits');
 const usersRouter = require('./routes/users');
+const ordersRoutes = require('./routes/orders')
 
 app.use(cors());
 app.use(bodyParser.json());
 // feature routes
 app.use('/kits', kitsRouter);
 app.use('/users', usersRouter);
+app.use('/orders', ordersRoutes)
 //
 
 app.get('/', (req, res) => {
   res.send({ text: 'Hello!' });
 });
 
-app.listen(process.env.SERVER_PORT || 3000, () => {
+app.listen(process.env.SERVER_PORT || 8080, () => {
   console.log(
-    'SERVER RUNNING...' + `http://localhost:${process.env.SERVER_PORT}/`
+    'SERVER RUNNING...' + `http://localhost:${process.env.SERVER_PORT || 8080}/`
   );
 });
