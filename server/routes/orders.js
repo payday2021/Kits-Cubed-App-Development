1829a33c8f6e921ee1c427c8b59dde662ba8a64b
payday2021 <dylancratersun@gmail.com>
2023-10-19 19:02:24 -0700

    Add server route for orders

---
+++ server/routes/orders.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router()
+const orders = require('../services/orders');
+
+
+
+router.get('/', (req,res) => {
+    res.send('hello from orders');
+    console.log('orderRouterRoot');
+})
+
+router.post('/add', function(req, res) {
+    try {
+        res.json(orders.insertOrder(req.body));
+      } catch (error) {
+        console.error(':( ERROR ADDING ORDER', error.message);
+      } 
+     //res.send('post order route')
+})
+
+router.get('/all', (req,res) => {
+  try {
+    res.json(orders.getAllOrders());
+  } catch (error) {
+    console.error(':( ERROR GETTING ALL ORDERS', error.message);
+  } 
+})
+
+module.exports = router;
