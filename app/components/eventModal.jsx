4a3f7f4e47bd8b8e2f405e1fa3c03b3927fdc59b
payday2021 <dylancratersun@gmail.com>
2023-10-30 17:23:03 -0700

    ADD user signup for events modal

--- app/components/eventModal.jsx
+++ app/components/eventModal.jsx
@@ -1,15 +1,24 @@
 import { View, Text, ScrollView, Button, Modal } from 'react-native'
+import { endEvent } from 'react-native/Libraries/Performance/Systrace';
+import { useEventSignupMutation } from '../features/api/apiSlice';
 
 export const EventModal = ({visible, onClose, event}) => {
+    const [userSignup] = useEventSignupMutation();
+
 
     const closeButtonPressed = () => {
-        onClose(0);
+        onClose();
+    }
+
+    const onRegisterButtonPressed = () => {
+        userSignup({event_id: event.event_id, event_name: event.name, participant: "Dylan"});
     }
 
     return(
         <Modal visible = {visible}>
             <View>
                 <Text>Hey</Text>
+                <Button title = "Register" onPress = {onRegisterButtonPressed}/>
                 <Button title = "Close" onPress = {closeButtonPressed}/>
             </View>
         </Modal>
