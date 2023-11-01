46257803b2a9a415592202bf444fb06b91cd892f
payday2021 <dylancratersun@gmail.com>
2023-10-31 19:56:27 -0700

    Add Event Details Page for Signup

--- app/components/eventModal.jsx
+++ app/components/eventModal.jsx
@@ -1,9 +1,17 @@
 import { View, Text, ScrollView, Button, Modal } from 'react-native'
-import { endEvent } from 'react-native/Libraries/Performance/Systrace';
 import { useEventSignupMutation } from '../features/api/apiSlice';
+import { useEffect, useState } from 'react';
+import { useIsUserRegisteredQuery } from '../features/api/apiSlice';
 
 export const EventModal = ({visible, onClose, event}) => {
     const [userSignup] = useEventSignupMutation();
+
+    const { data: isRegistered, error, isLoading } = useIsUserRegisteredQuery({ event_id: event.event_id, participant: "Dylan" });
+
+    useEffect(() => {
+        console.log('Component re-rendered');
+        console.log(isRegistered)
+      }, [isRegistered]);
 
 
     const closeButtonPressed = () => {
@@ -12,13 +20,20 @@ export const EventModal = ({visible, onClose, event}) => {
 
     const onRegisterButtonPressed = () => {
         userSignup({event_id: event.event_id, event_name: event.name, participant: "Dylan"});
+        console.log("you have registered!")
     }
+
 
     return(
         <Modal visible = {visible}>
             <View>
-                <Text>Hey</Text>
-                <Button title = "Register" onPress = {onRegisterButtonPressed}/>
+                <Text>{event.name}</Text>
+                <Text>{event.desc}</Text>
+                {!isLoading && !error ? (
+                    isRegistered.isRegistered === true
+                        ? <Text>You have already registered for this event.</Text>
+                        : <Button title="Register" onPress={onRegisterButtonPressed} />
+                ) : null}
                 <Button title = "Close" onPress = {closeButtonPressed}/>
             </View>
         </Modal>
