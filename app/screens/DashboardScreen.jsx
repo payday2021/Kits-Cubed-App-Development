4b5116e195a3ace24b42aa804be21be4a99cc3d6
nexusboyko <alexboyko@outlook.com>
2023-12-29 14:22:45 -0800

    Add user name navigation buttons to dashboard

--- app/screens/DashboardScreen.jsx
+++ app/screens/DashboardScreen.jsx
@@ -10,103 +10,30 @@ import {
   Button
 } from 'react-native';
 import { connect, useDispatch } from 'react-redux';
-import { getAllKits, getAllKitTypes } from '../features/kits/kitsSlice';
-import AppButton from '../components/Button';
-import { KitsList } from '../components/KitsList';
-import { CartModal } from '../components/cartModal';
 
 const DashboardScreen = (props) => {
   const dispatch = useDispatch();
-  const [list, setList] = useState([]);
-  const [types, setTypes] = useState([]);
-
-  const [cartModalIsVisible, setCartModalIsVisible] = useState(false);
-
-  useEffect(() => {
-    dispatch(getAllKits());
-    dispatch(getAllKitTypes());
-  }, []);
-
-  useEffect(() => {
-    setList(props.list);
-    setTypes(props.types);
-  }, [props.list, props.types]);
-
-  const filterByKitType = (type) => {
-    const newList = props.list.filter((kit) => kit.KitTypeName === type);
-
-    setList(newList);
-  }
-
-  // const filterKits = (kitType) => {
-  //   console.log('LIST', props.list);
-  //   const filteredData = props.list.filter((item) =>
-  //     item.typeK.toLowerCase().includes(kitType.toLowerCase())
-  //   );
-  //   setList(filteredData);
-  // };
 
   return (
     <SafeAreaView style={styles.container}>
       <Text>Dashboard</Text>
-      {/* <View style={styles.filter}>
-        <AppButton
-          title="electricity"
-          onPress={() => {
-            filterKits('electricity');
-          }}
-        ></AppButton>
-        <AppButton
-          title="mechanics"
-          onPress={() => {
-            filterKits('mechanics');
-          }}
-        ></AppButton>
-      </View> */}
       <ScrollView>
-        {/* <View>
-          {list ? <KitsList kitlist={list}></KitsList> : <Text>No kits</Text>}
-        </View> */}
-        <View style={styles.filter}>
+        <View>
+          <Text style={{ fontSize: 20 }}>Hi there, {props.userName}.</Text>
           <Button
-            title="All"
-            onPress={() => setList(props.list)}
+            title="Kits"
+            onPress={() => props.navigation.push('Kits Menu')}
           ></Button>
-          {
-            types && (
-              types.map((type) => {
-                return (
-                  <Button
-                    key={type.KitTypeID}
-                    title={type.KitTypeName}
-                    onPress={() => filterByKitType(type.KitTypeName)}
-                  ></Button>
-                );
-              })
-            )
-          }
+          <Button
+            title="My Cart"
+            onPress={() => props.navigation.push('Cart Menu')}
+          ></Button>
+          <Button
+            title="Events"
+            onPress={() => props.navigation.push('Events Menu')}
+          ></Button>
         </View>
-        {
-          list ? (
-            list.map((kit) => {
-              return (
-                <View key={kit.KitID}>
-                  <Text>{kit.KitName}</Text>
-                  <Text>{kit.KitDesc}</Text>
-                  <Text>({kit.KitTypeName})</Text>
-                </View>
-              );
-            })
-          ) : (
-            <Text>No kits</Text>
-          )
-        }
       </ScrollView>
-      {/* <Button title="view cart" onPress={() => setCartModalIsVisible(true)} /> */}
-      {/* <CartModal
-        visible={cartModalIsVisible}
-        onClose={() => setCartModalIsVisible(null)}
-      /> */}
     </SafeAreaView>
   );
 };
@@ -117,20 +44,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: StatusBar.currentHeight,
-    width: Dimensions.get('screen').width
+    width: Dimensions.get('screen').width,
   },
-  filter: {
-    flex: 1,
-    flexDirection: 'row',
-    gap: 20
-  }
 });
 
 const mapStateToProps = (state) => {
   return {
-    types: state.kits.types, 
-    list: state.kits.list, 
-    status: state.kits.status 
+    userName: state.auth.name,
   };
 };
 
