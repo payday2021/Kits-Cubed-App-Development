11018da2a2492e50e4f3b59360d48e833194da8e
nexusboyko <alexboyko@outlook.com>
2023-12-30 21:21:22 -0800

    Add events screen UI for all and user events

--- app/screens/EventsMenuScreen.jsx
+++ app/screens/EventsMenuScreen.jsx
@@ -10,19 +10,73 @@ import {
   Button
 } from 'react-native';
 import { connect, useDispatch } from 'react-redux';
+import {
+  getAllEvents,
+  getUserEvents,
+  addUserToEvent
+} from '../features/events/eventsSlice';
 
 const EventsMenuScreen = (props) => {
   const dispatch = useDispatch();
+  const [list, setList] = useState([]);
 
   useEffect(() => {
-    // dispatch();
+    dispatch(getAllEvents());
   }, []);
+
+  useEffect(() => {
+    setList(props.list);
+  }, [props.list]);
+
+  const fetchAllEvents = () => {
+    dispatch(getAllEvents());
+  }
+
+  const fetchUserEvents = () => {
+    dispatch(getUserEvents({
+      id: props.id
+    }));
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <Text>Events</Text>
       <ScrollView>
-        
+        <View style={styles.filter}>
+          <Button
+            title="All Events"
+            onPress={() => fetchAllEvents()}
+          ></Button>
+          <Button
+            title="My Events"
+            onPress={() => fetchUserEvents()}
+          ></Button>
+        </View>
+        <View>
+          {
+            list ? (
+              list.map((event) => {
+                return (
+                  <View key={event.EventID}>
+                    <Text>{event.EventName}</Text>
+                    <Text>{event.EventDate}</Text>
+                    <Text>({event.EventDesc})</Text>
+                    <Text>({event.EventLocation})</Text>
+                    <Button
+                      title="Add"
+                      onPress={() => dispatch(addUserToEvent({
+                        id: props.id,
+                        eventId: event.EventID
+                      }))}
+                    ></Button>
+                  </View>
+                );
+              })
+            ) : (
+              <Text>No events</Text>
+            )
+        }
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -35,12 +89,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: StatusBar.currentHeight,
     width: Dimensions.get('screen').width
+  },
+  filter: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: 20
   }
 });
 
 const mapStateToProps = (state) => {
   return {
-    
+    id: state.auth.id,
+    list: state.events.list,
+    status: state.events.status
   };
 };
 
